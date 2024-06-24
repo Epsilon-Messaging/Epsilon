9d583bc73932a5108492320ef0923d28f43592e3
Peaches_MLG <PeachesMLG100@gmail.com>
2024-06-24 09:49:42 +0100

    Added more sonarqube stuff

--- .buildkite/scripts/sonarqube.sh
+++ .buildkite/scripts/sonarqube.sh
@@ -1,12 +1,23 @@
 dotnet tool restore
 echo '--- :sonarqube: Running Sonarqube'
 
-dotnet sonarscanner begin \
-  /o:"epsilon-messaging" \
-  /k:"Epsilon-Messaging_Epsilon" \
-  /d:sonar.host.url="https://sonarcloud.io" \
-  /d:sonar.token="$SONARQUBE_TOKEN"
+if [[ "${}BUILDKITE_PULL_REQUEST}" == "false" ]]; then
+  dotnet sonarscanner begin \
+    /o:"epsilon-messaging" \
+    /k:"Epsilon-Messaging_Epsilon" \
+    /d:sonar.host.url="https://sonarcloud.io" \
+    /d:sonar.token="$SONARQUBE_TOKEN"
+else
+  dotnet sonarscanner begin \
+    /o:"epsilon-messaging" \
+    /k:"Epsilon-Messaging_Epsilon" \
+    /d:sonar.host.url="https://sonarcloud.io" \
+    /d:sonar.token="$SONARQUBE_TOKEN" \
+    /d:sonar.pullrequest.branch="$BUILDKITE_BRANCH" \
+    /d:sonar.pullrequest.key="${BUILDKITE_PULL_REQUEST}" \
+    /d:sonar.pullrequest.base="${BUILDKITE_PULL_REQUEST_BASE_BRANCH}"
+fi
   
-  dotnet build
-  
-  dotnet sonarscanner end /d:sonar.token="$SONARQUBE_TOKEN"
+dotnet build
+
+dotnet sonarscanner end /d:sonar.token="$SONARQUBE_TOKEN"
