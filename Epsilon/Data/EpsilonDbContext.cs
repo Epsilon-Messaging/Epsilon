f9549ec7e9780d22be7e79105d1c08080cdaa2f9
Tiqur <trevorbrage02@gmail.com>
2024-06-24 13:48:15 -0700

    Add packages and setup DB context

---
+++ Epsilon/Data/EpsilonDbContext.cs
@@ -0,0 +1,16 @@
+using Epsilon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Epsilon.Data
+{
+    public class EpsilonDbContext : DbContext
+    {
+        public EpsilonDbContext(DbContextOptions<EpsilonDbContext> options)
+        {
+                       
+        }
+        
+        public DbSet<Message> Messages { get; set; } 
+        public DbSet<User> Users { get; set; } 
+    }
+}
