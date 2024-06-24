ac45c212e1223ead9b4eb265d377b6e264178faa
Tiqur <trevorbrage02@gmail.com>
2024-06-24 14:06:30 -0700

    Added data annotations to models

--- Epsilon/Models/Message.cs
+++ Epsilon/Models/Message.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Epsilon.Models
 {
     public class Message
     {
-        // Hash of senders's public key
+       [Key]
+       public int? MessageId { get; set; }
+        
+       // Hash of senders's public key
+       [Required]
+       [ForeignKey("User")]
        public byte[]? SenderId { get; set; }
-        // Hash of recipient's public key
+        
+       // Hash of recipient's public key
+       [Required]
+       [ForeignKey("User")]
        public byte[]? RecipientId { get; set; }
+       
+       [Required]
        public byte[]? EncryptedMessage { get; set; }
+       
+       [Required]
        public DateTime? CreatedAt { get; set; }
     }
 }
--- Epsilon/Models/User.cs
+++ Epsilon/Models/User.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Epsilon.Models
 {
     public class User
     { 
-        // Hash of user's public key
+       // Hash of user's public key
+       [Key]
        public byte[]? Id { get; set; }
+       
+       [Required]
        public byte[]? PublicKey { get; set; }
+       
        // Can we possibly encrypt this such that only the sender and receiver can view this?
+       [Required]
+       [StringLength(16)]
        public string? Username { get; set; }
     }
 }
