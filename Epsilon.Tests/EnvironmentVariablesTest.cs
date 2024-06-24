683f6a1b8fd0baf0b6d100e4211f39b43104dbde
Peaches_MLG <PeachesMLG100@gmail.com>
2024-06-24 12:43:59 +0100

    Added EnvironmentVariablesTest.cs

---
+++ Epsilon.Tests/EnvironmentVariablesTest.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using Xunit;
+
+using static Epsilon.EnvironmentVariables;
+
+namespace Epsilon.Tests;
+
+public class EnvironmentVariablesTest
+{
+
+    [Fact]
+    public void ASPNETCORE_ENVIRONMENT_ShouldReturnEnvironment_WhenItsSet()
+    {
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+        ASPNETCORE_ENVIRONMENT.Should().Be("Development");
+    }
+
+    [Fact]
+    public void ASPNETCORE_ENVIRONMENT_ShouldThrowAnError_WhenItsNotSet()
+    {
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", null);
+        
+        var action = () => ASPNETCORE_ENVIRONMENT;
+        action.Should().Throw<ArgumentException>($"Environment Variable ASPNETCORE_ENVIRONMENT is not set");
+    }
+    
+}
