724973840c826239c65a78b64cd4e3fb87d7ed72
Peaches_MLG <PeachesMLG100@gmail.com>
2024-06-24 12:38:15 +0100

    Added Serilog + Cleanup

---
+++ Epsilon/EnvironmentVariables.cs
@@ -0,0 +1,15 @@
+using Environment = System.Environment;
+
+namespace Epsilon;
+
+public static class EnvironmentVariables
+{
+    public static string ASPNETCORE_ENVIRONMENT => GetEnvironmentVariable(nameof(ASPNETCORE_ENVIRONMENT));
+
+    private static string GetEnvironmentVariable(string environmentVariable)
+    {
+        return Environment.GetEnvironmentVariable(environmentVariable) ??
+               throw new ArgumentException($"Environment Variable ${environmentVariable} is not set",
+                   nameof(environmentVariable));
+    }
+}
